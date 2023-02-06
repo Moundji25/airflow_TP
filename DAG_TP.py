60ff4046f7c60a6901431072dd00ffde8d66a873
Moundji25 <moundjibel@hotmail.com>
2023-02-06 15:59:25 +0100

    modifs commit

--- DAG_TP.py
+++ DAG_TP.py
@@ -5,23 +5,46 @@ from airflow.operators.python_operator import PythonOperator
 def read_json_file(**kwargs):
     # Code pour lire le fichier JSON et parcourir les éléments
     # ...
+    print("JSON FILE TASK")
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+        return data
 
 # Définir les quatre tâches qui effectuent les appels aux services Web
 def call_webservice_1(**kwargs):
     # Code pour effectuer l'appel au service Web 1
     # ...
+    print("WEBSERVICE 1")
+
 
 def call_webservice_2(**kwargs):
     # Code pour effectuer l'appel au service Web 2
     # ...
+    print("WEBSERVICE 2")
+
 
 def call_webservice_3(**kwargs):
     # Code pour effectuer l'appel au service Web 3
     # ...
+    print("WEBSERVICE 3")
+    # Créer un client à partir du lien WSDL
+    client = Client(wsdl_url)
+
+    # Appeler la méthode du service web en passant les paramètres
+    result = client.service.call_method(parameters)
+
+    # Afficher le résultat
+    print(result)
+
 
 def call_webservice_4(**kwargs):
     # Code pour effectuer l'appel au service Web 4
     # ...
+    print("WEBSERVICE 4")
+
+# Exemple d'utilisation de la tâche
+wsdl_url = "http://example.com/service.wsdl"
+parameters = {"param1": "value1", "param2": "value2"}
 
 # Définir le DAG
 dag = DAG(
@@ -49,5 +72,5 @@ call_webservice_2_task = PythonOperator(
     dag=dag
 )
 
-call_webservice_3_task = PythonOperator(
-    task
+
+call_webservice_task(wsdl_url, parameters,python_callable=call_webservice_2,dag=dag)
