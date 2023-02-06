dbd057d5e5a9397481ded02bd94138a81f6f1196
Moundji25 <moundjibel@hotmail.com>
2023-02-06 23:06:39 +0100

    exemple_DAG_4

--- DAG_TP_2.py
+++ DAG_TP_2.py
@@ -1,18 +1,75 @@
-from datetime import datetime
-
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
-# A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
+from airflow.operators.python_operator import PythonOperator
+import json
+from datetime import datetime, timedelta
 
-    # Tasks are represented as operators
-    hello = BashOperator(task_id="hello", bash_command="echo hello")
+# Définir la tâche maîtresse qui lit le fichier JSON et parcourt les éléments
+def read_json_file(file_path, **kwargs):
+    # Code pour lire le fichier JSON et parcourir les éléments
+    # ...
+    print("JSON FILE TASK")
+    '''with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+        return data'''
 
-    @task()
-    def airflow():
-        print("airflow")
+# Définir les quatre tâches qui effectuent les appels aux services Web
+def call_webservice_1(**kwargs):
+    # Code pour effectuer l'appel au service Web 1
+    # ...
+    print("WEBSERVICE 1")
 
-    # Set dependencies between tasks
-    hello >> airflow()
+def call_webservice_2(**kwargs):
+    # Code pour effectuer l'appel au service Web 2
+    # ...
+    print("WEBSERVICE 2")
+
+def call_webservice_3(wsdl_url, parameters, **kwargs):
+    # Code pour effectuer l'appel au service Web 3
+    # ...
+    print("WEBSERVICE 3")
+
+# Définir le DAG
+dag = DAG(
+    dag_id='example_dag',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=timedelta(days=1)
+)
+
+call_webservice_3_task = PythonOperator(
+    task_id='call_webservice_3_task',
+    python_callable=call_webservice_3,
+    #op_args=[wsdl_url, parameters],
+    dag=dag
+)
+
+call_webservice_2_task = PythonOperator(
+    task_id='call_webservice_2_task',
+    python_callable=call_webservice_2,
+    #op_args=[wsdl_url, parameters],
+    dag=dag
+)
+
+call_webservice_1_task = PythonOperator(
+    task_id='call_webservice_1_task',
+    python_callable=call_webservice_1,
+    #op_args=[wsdl_url, parameters],
+    dag=dag
+)
+
+call_webservice_1_task = PythonOperator(
+    task_id='call_webservice_1_task',
+    python_callable=call_webservice_1,
+    #op_args=[wsdl_url, parameters],
+    dag=dag
+)
+
+read_json_task = PythonOperator(
+    task_id='read_json_task',
+    python_callable=read_json_file,
+    dag=dag
+)
+
+read_json_task >> call_webservice_1_task >> call_webservice_2_task >> call_webservice_3_task
